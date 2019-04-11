b428de9c06ca9f137799ee058230767af4be1522
Galit <glukin@mit.edu>
2019-04-10 20:23:08 -0400

    including all files

--- DataProcessing/processAttendance.py
+++ DataProcessing/processAttendance.py
@@ -1,15 +1,17 @@
 import pandas as pd
+import glob
 
 attendanceList = []
-months = 8
-for t in range(months):
-    temp = pd.read_excel('../Data/raw/ScheduleAtAGlance Report 10-1-2018 - 10-31-2018.xlsx')
+files = glob.glob('../Data/raw/' + "/*.xlsx")
+
+for f in files:
+    temp = pd.read_excel(f)
     temp = temp.loc[:,['Date', 'Start time', 'End time', 'Description', 'Staff', 'Client ID', 'Status']]
     attendanceList.append(temp)
 
 attendance = pd.concat(attendanceList)
 print(len(attendance))
-attendance = attendance.loc[attendance['Status'] == 'Signed in']
+attendance = attendance.loc[(attendance['Status'] == 'Signed in') | (attendance['Status'] == 'Reserved')]
 print(len(attendance))
 attendance = attendance.loc[:,['Date', 'Start time', 'End time', 'Description', 'Staff', 'Client ID']]
 attendance.to_csv('../Data/processed/attendance.csv', index = False)
