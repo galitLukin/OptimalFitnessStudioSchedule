0531b9f43362df3a7e496c55068dd44aa2222534
Galit <glukin@mit.edu>
2019-04-11 19:33:04 -0400

    look at data to filter out

--- DataProcessing/processAttendance.py
+++ DataProcessing/processAttendance.py
@@ -11,7 +11,21 @@ for f in files:
 
 attendance = pd.concat(attendanceList)
 print(len(attendance))
+#take only users that arrived
 attendance = attendance.loc[(attendance['Status'] == 'Signed in') | (attendance['Status'] == 'Reserved')]
 print(len(attendance))
+#take only classes being offered now
+temp = attendance.loc[:,['Description','Date','Start time']].drop_duplicates()
+print(temp.groupby('Description').count())
+# attendance = attendance.loc[(attendance['Description'] == 'HOT HATHA SCULPT') | (attendance['Description'] == ''INFERNO HOT PILATES'')\
+#                             | (attendance['Description'] == 'CLASSIC 90') | (attendance['Description'] == 'Reserved')\
+#                             | ]
+#take only instructors working now
+temp = attendance.loc[:,['Description','Date','Start time','Staff']].drop_duplicates()
+print(temp.groupby('Staff').count())
 attendance = attendance.loc[:,['Date', 'Start time', 'End time', 'Description', 'Staff', 'Client ID']]
 attendance.to_csv('../Data/processed/attendance.csv', index = False)
+
+
+
+#Assumptions:
