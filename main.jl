07ff33db51c2a2c75b762d3a2dbbc62855f39cf0
Galit <glukin@mit.edu>
2019-05-06 20:07:44 -0400

    fixing uncertainty sets

--- main.jl
+++ main.jl
@@ -13,55 +13,28 @@ U = 55
 
 # A is a vector of uncertainty matrices
 allAs = []
-allSubAsLower = []
-allSubAsUpper = []
 newAs = []
-newSubAsLower = []
-newSubAsUpper = []
 firstA = firstUncertainty()
 push!(newAs,firstA)
-initiateSubAs(allSubAsLower, firstA)
-initiateSubAs(allSubAsUpper, firstA)
-for d in 1:D
-    for t in 1:T
-        for c in 1:C
-            for i in 1:I
-                push!(newSubAsLower,firstA[d,t,c,i])
-                push!(newSubAsUpper,firstA[d,t,c,i])
-            end
-        end
-    end
-end
 println("got A")
 x = 0
 ub = 0
 iterations = 0
 vals = []
-while newAs != [] || lowerIsEmpty == false || upperIsEmpty == false
+while newAs != []
     for nextA in newAs
         push!(allAs,nextA)
         s = size(allAs,1)
         println("length of allAs: $s")
     end
-    x, ub = getSchedule(allAs, allSubAsLower, allSubAsUpper)
+    x, ub = getSchedule(allAs)
     push!(vals,ub)
     println("Got schedule, UB: $ub")
     if isnan(ub)
         break
     else
-        newAs, newSubAsLower, newSubAsUpper = buildUncertainties(ub,x)
+        newAs = buildUncertainties(ub,x)
         s1 = size(newAs,1)
-        s2 = size(newSubAsLower,1)
-        s3 = size(newSubAsUpper,1)
-        println("lower violated")
-        lowerIsEmpty = isSubEmpty(newSubAsLower)
-        println("upper violated")
-        upperIsEmpty = isSubEmpty(newSubAsUpper)
-        println("size of As: $s1, $s2-isEmpty: $lowerIsEmpty, $s3-isEmpty: $upperIsEmpty")
-        println("poplating lower")
-        populateSubAs(allSubAsLower, newSubAsLower)
-        println("poplating upper")
-        populateSubAs(allSubAsUpper, newSubAsUpper)
     end
     iterations = iterations + 1
 end
--- uncertainty.jl
+++ uncertainty.jl
@@ -1,18 +1,5 @@
 using DataFrames, JuMP
 
-function firstUncertaintyOrig()
-    lastWeekSchedule = readtable("Data/output/attendanceLastWeekIndex.csv", header=true, makefactors=true)
-    a = zeros(Int8, D,T,C,I)
-    for k in 1:size(lastWeekSchedule, 1)
-        d = lastWeekSchedule[k, :WeekDay]
-        t = lastWeekSchedule[k, :StartTime]
-        c = lastWeekSchedule[k, :Description]
-        i = lastWeekSchedule[k, :Staff]
-        a[d,t,c,i] = lastWeekSchedule[k, :Arrivals]
-    end
-    return a
-end
-
 function firstUncertainty()
     minVals, maxVals, minWeekly, maxWeekly, dailyMin, dailyMax = calcRanges()
     a = zeros(Int64, D,T,C,I)
@@ -21,10 +8,10 @@ function firstUncertainty()
             for c in 1:C
                 for i in 1:I
                     mi, ma = minVals[d,t,c,i], maxVals[d,t,c,i]
-                    a[d,t,c,i] = Int64(floor((mi + ma)/2))
-                    # if ma > 0
-                    #     a[d,t,c,i] = 55#Int64(floor((mi + ma)/2))
-                    # end
+                    #a[d,t,c,i] = Int64(floor((mi + ma)/2))
+                    if ma > 0
+                        a[d,t,c,i] = 55#Int64(floor((mi + ma)/2))
+                    end
                 end
             end
         end
@@ -67,74 +54,12 @@ end
 function buildUncertainties(obj,x)
     minVals, maxVals, minWeekly, maxWeekly, dailyMin, dailyMax = calcRanges()
     As = []
-    AsLower = []
-    AsUpper = []
     i = 1
     feasible, A, objective = wcArrivals(obj, x, minVals, maxVals, minWeekly, maxWeekly, dailyMin, dailyMax)
     if ~feasible && objective >= 1
         push!(As,A)
     end
-    for d in 1:D
-        for t in 1:T
-            for c in 1:C
-                for i in 1:I
-                    mi, ma = minVals[d,t,c,i], maxVals[d,t,c,i]
-                    feasible, A = wcLB(x[d,t,c,i],mi,ma)
-                    if ~feasible
-                        push!(AsLower,A)
-                    else
-                        push!(AsLower,-1)
-                    end
-                end
-            end
-        end
-    end
-    for d in 1:D
-        for t in 1:T
-            for c in 1:C
-                for i in 1:I
-                    mi, ma = minVals[d,t,c,i], maxVals[d,t,c,i]
-                    feasible, A = wcUB(x[d,t,c,i],mi,ma)
-                    if ~feasible
-                        push!(AsUpper,A)
-                    else
-                        push!(AsUpper,-1)
-                    end
-                end
-            end
-        end
-    end
-    return As, AsLower, AsUpper
-end
-
-function wcLB(x,mi,ma)
-    wc = Model(solver=GurobiSolver(OutputFlag=0))
-    @variable(wc, A>=0)
-    @objective(wc, Max, L*x - A)
-    @constraint(wc, A >= mi)
-    @constraint(wc, A <= ma)
-    solve(wc)
-    aVal = getvalue(A)
-    objective = getobjectivevalue(wc)
-    if objective > 0
-        return false, aVal
-    end
-    return true, aVal
-end
-
-function wcUB(x,mi,ma)
-    wc = Model(solver=GurobiSolver(OutputFlag=0))
-    @variable(wc, A>=0)
-    @objective(wc, Max, A*x - U)
-    @constraint(wc, A >= mi)
-    @constraint(wc, A <= ma)
-    solve(wc)
-    aVal = getvalue(A)
-    objective = getobjectivevalue(wc)
-    if objective > 0
-        return false, aVal
-    end
-    return true, aVal
+    return As
 end
 
 function wcArrivals(obj, x, minVals, maxVals, minWeekly, maxWeekly, dailyMin, dailyMax)
@@ -144,16 +69,16 @@ function wcArrivals(obj, x, minVals, maxVals, minWeekly, maxWeekly, dailyMin, da
     @variable(wc, A[1:D,1:T,1:C,1:I]>=0)
 
     @objective(wc, Max, obj - sum(A[d,t,c,i]*x[d,t,c,i] for d=1:D, t=1:T, c=1:C, i=1:I))
-    # for d in 1:D
-    #     for t in 1:T
-    #         for c in 1:C
-    #             for i in 1:I
-    #                 @constraint(wc, A[d,t,c,i] >= minVals[d,t,c,i])
-    #                 @constraint(wc, A[d,t,c,i] <= maxVals[d,t,c,i])
-    #             end
-    #         end
-    #     end
-    # end
+    for d in 1:D
+        for t in 1:T
+            for c in 1:C
+                for i in 1:I
+                    @constraint(wc, A[d,t,c,i] >= minVals[d,t,c,i])
+                    @constraint(wc, A[d,t,c,i] <= maxVals[d,t,c,i])
+                end
+            end
+        end
+    end
     for d in 1:D
         @constraint(wc, sum(A[d,t,c,i]*x[d,t,c,i] for t=1:T, c=1:C, i=1:I) >= dailyMin[d])
     end
